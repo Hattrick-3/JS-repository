f2bd0ac741a069b6a740fc99e8c5553ff0751a2e
rainer048 <37981586+rainer048@users.noreply.github.com>
2019-03-19 13:06:30 +0300

    Update script.js
    update codestyle, update "var" to "let", add const instead var

--- 1/script.js
+++ 1/script.js
@@ -1,14 +1,14 @@
-var myName = prompt('Введите Ваше имя');
-var mySurname = prompt('Введите Вашу фамилию');
-var myBirthYear = prompt('Введите Ваш год рождения');
+let myName = prompt('Введите Ваше имя');
+let mySurname = prompt('Введите Вашу фамилию');
+let myBirthYear = prompt('Введите Ваш год рождения');
 
-var currentYear = 2019;
-var age = currentYear - myBirthYear;
+const CURRENT_YEAR = 2019;
+let age = CURRENT_YEAR - myBirthYear;
 
 if (age < 20) {
   alert('Привет, ' + myName + ' ' + mySurname + '!');
 }
-else if(age >= 20 && age < 40){
+else if (age >= 20 && age < 40) {
   alert('Добрый день, ' + myName + ' ' + mySurname + '!');
 }
 else {
